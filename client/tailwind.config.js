ae5a1a98e90030211a5f6b93b618720db1508448
naga105 <87576962+naga105@users.noreply.github.com>
2022-01-09 14:05:04 +0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+      extend: {
+            backgroundColor: ["active"],
+      },
     colors: {
       transparent: "transparent",
       current: "currentColor",
