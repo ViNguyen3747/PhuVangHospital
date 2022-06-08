9e276a7e2356e1061a00ac40117229b888ad8331
naga115 <huy1152002@gmail.com>
2022-06-08 16:29:42 +0700

    change

--- client/src/components/User/index.js
+++ client/src/components/User/index.js
@@ -30,7 +30,7 @@ const Users = () => {
   return (
     <div className="place-items-center h-screen">
       <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="-my-2 sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="shadow overflow-hidden border-gray border-b-4 sm:rounded-lg">
               <table className="min-w-full divide-y divide-turquoise">
